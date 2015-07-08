f45fa97380b005f0db65e80c548e255239692cc9
erickarr <erickarr11@gmail.com>
2015-07-07 23:12:42 -0500

    Removed old file setup_git.sh

--- setup_git.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Script initiates git configuration 
-
-git config --global user.name erickarr
-
-git config --global user.email erickarr11@gmail.com
-
-git config --system core.editor vim
