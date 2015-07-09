1cc498c3a6fe1935d6ff4104da130ee075db3882
erickarr <erickarr11@gmail.com>
2015-07-08 23:21:53 -0500

    Made lots of progess on setup.sh

--- git_setup.sh
+++ git_setup.sh
@@ -7,3 +7,5 @@ git config --global user.name erickarr
 git config --global user.email erickarr11@gmail.com
 
 git config --system core.editor vim
+
+git config --global push.default simple
--- setup.sh
+++ setup.sh
@@ -1,10 +1,49 @@
 #!/bin/bash
 
+#Install pathogen mod and modules
+function pathogen_init {
+	printf "Ensure package 'curl' is already installed\n"
+
+	mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+	curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+	printf "Copying over listed modules\n"
+
+	#Iterate through a list of git clones to install the latest modules
+	while read line
+	do
+		#printf "Adding module from git repository %s\n" $line # | sed 's/.*\///'
+		cd ~/.vim/bundle/
+		$line
+	done < modules.config
+
+	#To be finished, prints the repository as desired, doesn't have full path to execute
+	#sed 's/.*\///' modules.config
+}
+
 #Put the .bashrc file in place
-printf "Making a backup copy of the current .bashrc file\n"
+function bashrc_init {
+	printf "Making a backup copy of the current .bashrc file\n"
 
-cp -v ~/.bashrc ~/.bashrc.backup
+	cp -v ~/.bashrc ~/.bashrc.backup
 
-printf "\nNew .bashrc file being copied to ~/.bashrc\n"
+	printf "Creating a hard link to the new bash file\n"
 
-cp -v ./.bashrc ~/.bashrc
+	ln -fv `pwd`/.bashrc ~/.bashrc 
+}
+
+#Put the .vimrc file in place
+function vimrc_init {
+	printf "\nMaking a backup copy of the current .vimrc file\n"
+
+	cp -v ~/.vimrc ~/.vimrc.backup
+
+	printf "Creating a hard link to the new vim configuration file\n"
+
+	ln -fv `pwd`/.vimrc ~/.vimrc 
+
+}
+
+#pathogen_init
+bashrc_init
+vimrc_init
